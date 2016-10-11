1be1863b31c128fbcd90d32cb7d4bc94457d9e20
Robert M Flight <r.flight79@gmail.com>
2016-10-11 11:37:33 -0400

    made the log-transformation function

--- R/normalization.R
+++ R/normalization.R
@@ -25,9 +25,35 @@ create_value <- function(mantissa, exponent){
 }
 
 
-#' log-transform
+#' log-transform data
 #'
 #' performs a log-transform while adding a small value to the data based on
 #' finding the smallest non-zero value in the data
 #'
-#' @param
+#' @param data_matrix the data to work with
+#' @param min_value the minimum value
+#' @param order_mag how many orders of magnitute smaller should min value be?
+#' @param log_fun what log function to use for the transformation
+#'
+#' @export
+#' @return matrix
+log_transform <- function(data_matrix, min_value = NULL, order_mag = 3, log_fun = log){
+  stopifnot(class(data_matrix) != "matrix")
+  stopifnot(class(data_matrix) != "numeric")
+
+  if (min(data_matrix) < 0) {
+    stop("Values less than zero detected, aborting!")
+  }
+
+  if (is.null(min_value)) {
+    min_value <- min(data_matrix[data_matrix > 0])
+  }
+
+  split_min <- extract(min_value)
+  split_min$exponent <- split_min$exponent - order_mag
+  add_value <- create_value(split_min$mantissa, split_min$exponent)
+
+  out_matrix <- data_matrix + add_value
+  log_matrix <- log_fun(out_matrix)
+  log_matrix
+}
