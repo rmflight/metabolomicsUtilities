3d7b69dcc10cdc100f8c5124905763c2aef8564d
Robert M Flight <r.flight79@gmail.com>
2016-10-21 13:21:38 -0400

    using either a vector or matrix for the log data

--- R/normalization.R
+++ R/normalization.R
@@ -30,30 +30,30 @@ create_value <- function(mantissa, exponent){
 #' performs a log-transform while adding a small value to the data based on
 #' finding the smallest non-zero value in the data
 #'
-#' @param data_matrix the data to work with
+#' @param data the data to work with
 #' @param min_value the minimum value
 #' @param order_mag how many orders of magnitute smaller should min value be?
 #' @param log_fun what log function to use for the transformation
 #'
 #' @export
 #' @return matrix
-log_with_min <- function(data_matrix, min_value = NULL, order_mag = 3, log_fun = log){
-  stopifnot(class(data_matrix) == "matrix")
+log_with_min <- function(data, min_value = NULL, order_mag = 3, log_fun = log){
+  #stopifnot(class(data_matrix) == "matrix")
   #stopifnot(class(data_matrix) == "numeric")
 
-  if (min(data_matrix) < 0) {
+  if (min(data) < 0) {
     stop("Values less than zero detected, aborting!")
   }
 
   if (is.null(min_value)) {
-    min_value <- min(data_matrix[data_matrix > 0])
+    min_value <- min(data[data > 0])
   }
 
   split_min <- extract(min_value)
   split_min$exponent <- split_min$exponent - order_mag
   add_value <- create_value(split_min$mantissa, split_min$exponent)
 
-  out_matrix <- data_matrix + add_value
-  log_matrix <- log_fun(out_matrix)
-  log_matrix
+  out_data <- data + add_value
+  log_data <- log_fun(out_data)
+  log_data
 }
